aa4abb51c3eb16440f321f61e06f47ec31d92ca1
BharaniDharan <bharani007dharan@gmail.com>
2024-11-24 16:38:09 +0530

    test

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"omEB4IGoFdekmeeVEnpk7T6lSZFVIIyLFvUk9WYwcpc=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fOoIAzJ590jmyAc3/yAEIZLBLRe92FeEOeZIMiBBnTo=\"\n}"
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['scxxvniczlyzyumuiohc.supabase.co'],
+  },
+  // Add any other necessary configurations
+}
+
+module.exports = nextConfig
